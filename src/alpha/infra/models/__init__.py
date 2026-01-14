8c61ddf83262ffee11507741238ec1388a37bdad
Bart Reijling <bart@reijling.eu>
2026-01-14 22:24:41 +0100

    feature: imports at root level

---
+++ src/alpha/infra/models/__init__.py
@@ -0,0 +1,15 @@
+from alpha.infra.models.filter_operators import And, Or, Not
+from alpha.infra.models.json_patch import JsonPatch
+from alpha.infra.models.order_by import OrderBy, Order
+from alpha.infra.models.search_filter import SearchFilter, Operator
+
+__all__ = [
+    "And",
+    "Or",
+    "Not",
+    "JsonPatch",
+    "OrderBy",
+    "Order",
+    "SearchFilter",
+    "Operator",
+]
