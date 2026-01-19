fa4fb6a4a58e7be50afc26b0936185dccb97ba3c
Bart Reijling <bart@reijling.eu>
2026-01-19 07:54:56 +0100

    fix: unused import

--- src/alpha/providers/oidc_provider.py
+++ src/alpha/providers/oidc_provider.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timezone
 from typing import Any, Mapping, Sequence, cast
 
 from alpha import exceptions
